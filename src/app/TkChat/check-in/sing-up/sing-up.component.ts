63830e9ce53d44251564e1f41c998f6d7612428c
the-zein7 <jhojanhr16@icloud.com>
2023-05-25 01:10:10 -0500

    intento 9999999

--- src/app/TkChat/check-in/sing-up/sing-up.component.ts
+++ src/app/TkChat/check-in/sing-up/sing-up.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.interface';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
+   selector: 'app/TkChat/check-in/sing-up',
    templateUrl: './sing-up.component.html',
    styleUrls: ['./sing-up.component.scss']
 })
 export class SignupComponent implements OnInit {
+
+   singupForm: FormGroup;
 
    user: User = {
       id: '',
@@ -19,6 +23,13 @@ export class SignupComponent implements OnInit {
    };
 
    constructor(private userServ: UserService, private router: Router) {
+      this.singupForm = new FormGroup({
+         nombres : new FormControl('', Validators.required),
+         apellidos : new FormControl('', Validators.required),
+         correo : new FormControl('', Validators.required),
+         password : new FormControl('', Validators.required),
+         samepass : new FormControl('', Validators.required)
+      })
    }
 
    ngOnInit(): void {
